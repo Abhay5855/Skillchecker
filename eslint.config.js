99a860f07b69cd11fc6773919e0845de30f1bfd0
Abhay5855 <ap3919529@gmail.com>
2024-12-02 00:33:27 +0530

    "Updated Prettier config, ESLint config, and React imports in App and main files"

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,13 @@ import pluginReact from 'eslint-plugin-react';
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
 	{ languageOptions: { globals: globals.browser } },
-	{ extends: ['plugin:react/recommended', 'airbnb', , 'prettier'] },
+	{
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 	pluginReact.configs.flat.recommended,
